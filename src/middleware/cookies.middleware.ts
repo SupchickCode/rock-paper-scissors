01e20805840aca387aa6e48c701eca4949ae3e3e
Your Name <you@example.com>
2022-04-17 16:28:16 +0300

    edit middleware

--- src/middleware/cookies.middleware.ts
+++ src/middleware/cookies.middleware.ts
@@ -1,21 +1,18 @@
-import * as express from 'express';
-
+import {Request, Response, NextFunction} from 'express';
 
 const getRandomStr = (): string => {
     return (Math.random() + 1).toString(36).substring(2);
 }
 
 const getRandomToken = (): string => {
-    return getRandomStr() + getRandomStr();
+    return getRandomStr() + getRandomStr() + getRandomStr();
 }
 
-const cookiesMiddleware = () => (request: express.Request, response: express.Response, next: express.NextFunction) => {
-    if (!request.cookies.user_id) {
-        // request.cookie('user_id', getRandomToken(),
-        //     { maxAge: 900000, httpOnly: true });
+const cookiesMiddleware = () => (request: Request, response: Response, next: NextFunction) => {
+    if (!request.cookies.guest_token) {
+        response.cookie('guest_token', getRandomToken(),
+            { maxAge: 900000, httpOnly: true });
     }
-
-    console.log(request.cookies.user_id);
 
     next();
 }

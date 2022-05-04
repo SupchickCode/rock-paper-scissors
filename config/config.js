51498f35cc5d1b6ed4cd50a537ef271e423f9833
Your Name <you@example.com>
2022-05-04 22:12:28 +0300

    edit t3 and example of .env

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
     "development": {
-        "username": process.env.PG_USER,
-        "password": process.env.PG_PASSWORD,
-        "database": process.env.PG_DATABASE,
-        "host": process.env.PG_HOST,
+        "username": process.env.DB_USER,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
         "dialect": "postgres"
     }
 }
--- src/config/database.ts
+++ src/config/database.ts
@@ -1,5 +1,5 @@
 require('dotenv').config();
 
-const databaseConfig: string = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
+const databaseConfig: string = `${process.env.DB_DIALECT}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 export default databaseConfig;
